d1944ca3e8b61214d15c183c04d9dd1c5f1bf196
Luke <lukecherniawski@gmail.com>
2025-02-09 21:31:36 -0700

    got sortof a working button

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-const array = {}
-const array2 = {}
+const link = ['../cozeyTea', '../mixed_messages','../fotomatic_broken' ];
+const projectName = ['Cozey Tea', 'Mixed Messages', 'Broken Website'];
+
+
+function insertElement(array1, array2){
+let i = 0;
+let button = document.querySelector('button');
+let works = document.createElement('a');
+ 
+works.href = array1[i];
+works.innerHTML = array2[i];
+document.getElementById('my_Projects').appendChild(works);
+button.onclick = function(){ 
+ works.href = array1[i++];
+ works.innerHTML = array2[i++];
+    
+}
+}
+
+insertElement(link, projectName);
+
+
